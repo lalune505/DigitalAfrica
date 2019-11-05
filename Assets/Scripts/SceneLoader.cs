ca6b5099e50911953506276ff9be1c4d1547d8ed
zhenyaprivet <zhenya@phygitalism.com>
2019-11-05 14:01:35 +0300

    New targets and scriptable object

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,14 +7,10 @@ using Vuforia;
 
 public class SceneLoader : MonoBehaviour
 {
+    public ScenePrefabsSet scenePrefabsSetAnimals;
     public static SceneLoader instance;
     private AsyncOperation _asyncOperation;
 
-    private bool IsReady = false;
-    
-    private const string AnimalsScenePath = "Assets/Scenes/AnimalsScene.unity";
-    private const string MasksScenePath = "";
-    
     private const string AnimalsARScenePath = "Assets/Scenes/AnimalsARScene.unity";
     private const string MasksARScenePath = "";
     
@@ -36,7 +32,6 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadAnimalsScene()
     {
-        SceneManager.LoadScene(AnimalsScenePath);
         StartCoroutine(LoadAsyncScene(AnimalsARScenePath));
     }
 
@@ -44,50 +39,26 @@ public class SceneLoader : MonoBehaviour
     {
         StartCoroutine(LoadAsyncScene(MasksARScenePath));
     }
-    
+
     private IEnumerator LoadAsyncScene(string scenePath)
     {
         yield return null;
 
-        Scene currentScene = SceneManager.GetActiveScene();
-
         _asyncOperation = SceneManager.LoadSceneAsync(scenePath, LoadSceneMode.Additive);
-        
+
         VuforiaRuntime.Instance.InitVuforia();
 
         _asyncOperation.allowSceneActivation = false;
-        
-         while (!_asyncOperation.isDone)
+
+        while (!_asyncOperation.isDone)
         {
             if (_asyncOperation.progress >= 0.9f && (uint) VuforiaRuntime.Instance.InitializationState > 0U)
             {
                 _asyncOperation.allowSceneActivation = true;
-                
             }
-            
+
             yield return null;
         }
-         
-        MoveGameObjectsToScene(SceneManager.GetSceneByPath(scenePath));
 
-        SceneManager.UnloadSceneAsync(currentScene);
-
-    }
-
-    private void MoveGameObjectsToScene(Scene sceneToMove)
-    {
-        TestTargetsController testTargetsController = FindObjectOfType<TestTargetsController>();
-        TargetsController targetsController = FindObjectOfType<TargetsController>();
-        
-        
-        for (var i = 0; i < targetsController.targets.Count; i++)
-        {
-             GameObject go = testTargetsController.testTargets[i].transform.GetChild(0).gameObject;
-             go.transform.parent = null;
-             SceneManager.MoveGameObjectToScene(go, sceneToMove);
-             
-             go.transform.SetParent(targetsController.targets[i].gameObject.transform);
-        }
-        
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/ScenePrefabsSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+[CreateAssetMenu(fileName = "ScenePrefabsSet", menuName = "ScriptableObjects/ScenePrefabs", order = 100)]
+public class ScenePrefabsSet : ScriptableObject
+{
+    [SerializeField]
+    public List<GameObject> targets;
+}
