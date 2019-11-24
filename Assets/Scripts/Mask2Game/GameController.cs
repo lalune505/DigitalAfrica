bf51a3127e231a6b16dcb3312f04f6b4b567ac6f
zhenyaprivet <zhenya@phygitalism.com>
2019-11-24 21:22:59 +0300

    GameController added

---
+++ Assets/Scripts/Mask2Game/GameController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Player player;
+
+    [SerializeField] private ScoreArea scoreArea;
+
+    [SerializeField] private List <GameObject> throwableGameObjects;
+    
+
+    private int _currentGoNumber = 0;
+
+    private float _resetTimer = 3f;
+
+    private bool _isFinished = true;
+
+    private List<GameObject> _currentRoundPrefabs;
+    
+    void Start()
+    {
+
+       StartGame();
+        
+    }
+    
+    void Update()
+    {
+        if (_isFinished) return;
+        
+        if (!player.IsHoldingBall())
+        {
+            _resetTimer -= Time.deltaTime;
+            if (_resetTimer <= 0f)
+            {
+                StartNewRound();
+            }
+        }
+
+    }
+    private void StartNewRound()
+    {
+        player.DestroyGO();
+        
+        if (scoreArea.collided)
+        {
+            scoreArea.collided = false;
+            if (_currentRoundPrefabs.Count > 1)
+            {
+               _currentRoundPrefabs.RemoveAt(_currentGoNumber);
+            }
+            else
+            {
+                _isFinished = true;
+                return;
+            }
+        }
+        
+        player.ConfigCurrentBall(Instantiate(GetRandomGameObject()));
+        _resetTimer = 3f;
+        
+        
+    }
+    private GameObject GetRandomGameObject()
+    {
+        _currentGoNumber = Random.Range(0, _currentRoundPrefabs.Count);
+        return _currentRoundPrefabs[_currentGoNumber];
+    }
+
+    public void StartGame()
+    {
+        _currentRoundPrefabs = throwableGameObjects;
+
+        _currentGoNumber = 0;
+        
+        player.gameObject.SetActive(true);
+
+        StartNewRound();
+        
+        _isFinished = false;
+    }
+
+    public void StopGame()
+    {
+        _isFinished = true;
+
+        player.DestroyGO();
+        
+        player.gameObject.SetActive(false);
+
+        _currentGoNumber = 0;
+
+    }
+
+    private void OnDisable()
+    {
+        StopGame();
+    }
+    
+    
+}
---
+++ Assets/Scripts/Mask2Game/Player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject _currentBall;
+    private Rigidbody _currentBallRigidBody;
+    private Camera  _camera;
+    
+    private float _ballDistanceZ = 2.25f;
+    private float _ballDistanceY = -1f;
+    private float _throwForce = 500f;
+
+    private bool _holding = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_holding)
+        {
+            var cameraTransform = _camera.transform;
+            var transformUp = cameraTransform.up;
+            var transformForward = cameraTransform.forward; 
+            
+            _currentBall.transform.position = cameraTransform.position + transformForward * _ballDistanceZ +
+                                             transformUp * _ballDistanceY;
+            if (
+         #if UNITY_EDITOR 
+                Input.GetMouseButtonDown(0) 
+         #elif UNITY_IOS || UNITY_ANDROID
+               Input.touchCount > 0  && Input.GetTouch(0).phase == TouchPhase.Began
+         #endif
+                )
+            {
+                Ray ray = Camera.main.ScreenPointToRay(
+                 #if UNITY_EDITOR
+                    Input.mousePosition);
+                    #elif UNITY_IOS || UNITY_ANDROID
+                     Input.GetTouch(0).position);
+                    #endif
+                if (Physics.Raycast(ray, out var hit, Mathf.Infinity, LayerMask.GetMask("Ball")))
+                {
+                    ThrowGameObject();
+                }
+            }
+        }
+    }
+
+    public void ConfigCurrentBall(GameObject ball)
+    {
+        _holding = true;        
+        _currentBall = ball;
+        _currentBallRigidBody = _currentBall.AddComponent<Rigidbody>();
+
+        _currentBallRigidBody.useGravity = false;
+    }
+
+    private void ThrowGameObject()
+    {
+        _holding = false;
+        _currentBallRigidBody.useGravity = true;
+        _currentBallRigidBody.AddForce(_camera.transform.forward * _throwForce);
+    }
+
+    public void DestroyGO()
+    {
+        if (_currentBall != null)
+        {
+            Destroy(_currentBall);
+        }
+    }
+    public bool IsHoldingBall()
+    {
+        return _holding;
+    }
+}
