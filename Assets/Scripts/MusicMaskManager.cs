79051c81cd401d9be0d2b7f9241aa63fc7678084
zhenyaprivet <zhenya@phygitalism.com>
2019-12-06 21:31:46 +0300

    Targets and Interactive content

---
+++ Assets/Scripts/MusicMaskManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMaskManager : MonoBehaviour, IUserInteractable
+{
+    public void HandleInputOccur(RaycastHit hit)
+    {
+        hit.collider.GetComponent<AudioSource>().Play();
+    }
+}
