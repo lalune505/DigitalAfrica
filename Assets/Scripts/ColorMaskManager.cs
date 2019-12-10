48735e6998f501e7f8d8776dae1b78a9af92cc7d
zhenyaprivet <zhenya@phygitalism.com>
2019-12-10 16:07:05 +0300

    Masks fixed

--- Assets/Scripts/ColorMaskManager.cs
+++ Assets/Scripts/ColorMaskManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ColorMaskManager : MonoBehaviour, IUserInteractable
 {
-    private readonly List<string> _triggers = new List<string>{"Touch1", "Touch2", "Touch3","Touch4", "Touch5", "Touch6", "Touch7"};
+    private readonly List<string> _triggers = new List<string>{"Touch1", "Touch2", "Touch3","Touch4", "Touch5", "Touch6"};
     public void HandleInputOccur(RaycastHit hit)
     {
         hit.collider.GetComponent<Animator>().SetTrigger(_triggers[Random.Range(0, _triggers.Count)]);
